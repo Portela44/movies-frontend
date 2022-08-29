b340c0857d9effe03e66d31a78ad4e3247b445aa
Guillem <guillem.mart7@gmail.com>
2022-08-29 17:35:19 +0200

    iterations 3 and 5 done (ups)

--- src/views/Edit.jsx
+++ src/views/Edit.jsx
@@ -1,22 +1,24 @@
-import React, {useState, useEffect, useNavigate} from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Edit() {
   const navigate = useNavigate();
   const {id} = useParams();
-  const [movie, setMovie] = useState(null)
+  const [movie, setMovie] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = axios.put(`http://localhost:8000/api/v1/movies/${id}`);
-      
-    } catch (error) {
-      console.error(error)
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/v1/movies/${id}`);
+        setMovie(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
-  
+    getData();
+  }, [id])
+
   const handleChange = (e) => {
     setMovie(prev => {
       return {
@@ -26,12 +28,31 @@ export default function Edit() {
     })
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.put(`http://localhost:8000/api/v1/movies/${id}`, movie);
+      navigate(`/movie/${response.data.data._id}`);      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div>
       <h2>Edit movie</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="Title" value={movie.title} onChange={handleChange}/>
-      </form>
+      {!movie && <p>Loading</p> }
+      {movie && (
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="title" placeholder="Title" value={movie.title} onChange={handleChange}/>
+          <input type="number" name="year" placeholder="Year" value={movie.year} onChange={handleChange}/>
+          <input type="text" name="director" placeholder="Director" value={movie.director} onChange={handleChange}/>
+          <input type="number" name="duration" placeholder="Duration" value={movie.duration} onChange={handleChange}/>
+          <input type="text" name="synopsis" placeholder="Synopsis" value={movie.synopsis} onChange={handleChange}/>
+          <input type="text" name="image" placeholder="Image" value={movie.image} onChange={handleChange}/>
+          <button type="submit">Save changes</button>
+        </form>
+      )}
     </div>
   )
 }
